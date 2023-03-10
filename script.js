1c49160cc02b56f376dd3b3da3e7c5ea92b5581e
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-10 19:49:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your code here
+class OutOfRangeError extends Error {
+	constructor(params) {
+		super(params);
+		this.name = "OutOfRangeError";
+	}
+}
+
+class InvalidExprError extends Error{
+	constructor(params) {
+		super(params);
+		this.name = "InvalidExprError";
+	} 
+} 
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener('click', (e)=>{
+	let input = document.getElementById("input").value;
+	let exprarray = input.split("");
+try {
+	if(!(exprarray[0] >= '0' && exprarray[0] <= '9'))
+		throw new OutOfRangeError("Expression should not start with invalid operator");
+	document.write(eval(input));
+	
+} catch (OutOfRangeError) {
+	document.write(OutOfRangeError.message);
+}
+
+
+	
+});
